49ed9ba9ca2efa40a13112a0c4578679bacc2d8a
vvovvikk <coin@mok.by>
2023-05-03 14:46:10 +0300

    ooop

--- src/data/state.js
+++ src/data/state.js
@@ -1,107 +1,124 @@
 import burunov from "../img/Sergey_Burunov.jpg";
 import tohatjan from "../img/HrantTokhatyan.jpg";
 import truhin from "../img/MihailTruhin.jpg";
-import rerenderTree from "../render";
 
 
-let state = {
+let store = {
 
-    profilePage: {
-        postData: [
-            { post: "Hi, world!", id: 1, likes: 2 },
-            { post: "HI`m Elon Mask, really", id: 2, likes: 8 },
-            {
-                post: "Give me some bitcoins",
-                id: 3,
-                likes: 10
-            },
+    _state: {
 
-        ],
-        newPostText: 'Hello',
+        profilePage: {
+            postData: [
+                { post: "Hi, world!", id: 1, likes: 2 },
+                { post: "HI`m Elon Mask, really", id: 2, likes: 8 },
+                {
+                    post: "Give me some bitcoins",
+                    id: 3,
+                    likes: 10
+                },
+
+            ],
+            newPostText: 'Hello',
+        },
+        dialogsPage: {
+            dialogNames: [
+                {
+                    name: 'Иван Иванов',
+                    id: 1
+                },
+                {
+                    name: 'Дональд Трамп',
+                    id: 2
+                },
+                {
+                    name: 'Билл Гейтс',
+                    id: 3
+                },
+            ],
+
+            dialogMsgs: [
+                {
+                    message: 'Привет, как дела?',
+                    id: 1
+                },
+                {
+                    message: 'Lorem ipsum dolor ',
+                    id: 2
+                },
+                {
+                    message: 'Жду мой новый чип',
+                    id: 3
+                },
+            ],
+            newMsg: 'Новое сообщение'
+        },
+        navBar: {
+            friends: [
+                {
+                    name: 'Сергей Бурунов',
+                    img: burunov,
+                    id: 1
+                },
+                {
+                    name: 'Грант Тохатян',
+                    img: tohatjan,
+                    id: 2
+                },
+                {
+                    name: 'Михаил Трухин',
+                    img: truhin,
+                    id: 3
+                },
+            ]
+        },
     },
-    dialogsPage: {
-        dialogNames: [
-            {
-                name: 'Иван Иванов',
-                id: 1
-            },
-            {
-                name: 'Дональд Трамп',
-                id: 2
-            },
-            {
-                name: 'Билл Гейтс',
-                id: 3
-            },
-        ],
 
-        dialogMsgs: [
-            {
-                message: 'Привет, как дела?',
-                id: 1
-            },
-            {
-                message: 'Lorem ipsum dolor ',
-                id: 2
-            },
-            {
-                message: 'Жду мой новый чип',
-                id: 3
-            },
-        ],
-        newMsg: 'Новое сообщение'
+    addPost(postText) {
+        let newPost = {
+            post: postText,
+            id: 4,
+            likes: 10
+        }
+
+        this._state.profilePage.postData.unshift(newPost)
+        this._state.profilePage.newPostText = ''
+        this.rerenderTree(this._state)
     },
-    navBar: {
-        friends: [
-            {
-                name: 'Сергей Бурунов',
-                img: burunov,
-                id: 1
-            },
-            {
-                name: 'Грант Тохатян',
-                img: tohatjan,
-                id: 2
-            },
-            {
-                name: 'Михаил Трухин',
-                img: truhin,
-                id: 3
-            },
-        ]
+
+    addMsg(msgText) {
+        let newMsg = {
+            message: msgText,
+            id: 4
+        }
+        this._state.dialogsPage.dialogMsgs.unshift(newMsg)
+        this._state.dialogsPage.newMsg = ''
+        this.rerenderTree(this._state)
     },
+
+    onChangePost(text) {
+        this._state.profilePage.newPostText = text
+        this.rerenderTree(this._state)
+    },
+
+    onChangeMsg(text) {
+        this._state.dialogsPage.newMsg = text
+        this.rerenderTree(this._state)
+    },
+
+    rerenderTree() {
+        console.log('Заглушка');
+    },
+
+    subscribe(observer) {
+        this.rerenderTree = observer
+    },
+
+    getState() {
+        return this._state
+    },
+
+
+
 }
 
-export let addPost = (postText) => {
-    let newPost = {
-        post: postText,
-        id: 4,
-        likes: 10
-    }
-
-    state.profilePage.postData.unshift(newPost)
-    state.profilePage.newPostText=''
-    rerenderTree(state)
-}
-
-export let addMsg = (msgText) => {
-    let newMsg = {
-        message: msgText,
-        id: 4
-    }
-    state.dialogsPage.dialogMsgs.unshift(newMsg)
-    state.dialogsPage.newMsg=''
-    rerenderTree(state)
-}
-
-export let onChangePost = (text) => {
-    state.profilePage.newPostText = text
-    rerenderTree(state)
-}
-
-export let onChangeMsg = (text) => {
-    state.dialogsPage.newMsg = text
-    rerenderTree(state)
-}
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import reportWebVitals from './reportWebVitals';
-import rerenderTree from "./render";
-import state from './data/state';
 
-rerenderTree(state)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import store from './data/state';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function rerenderTree(state) {
+  root.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={store.addPost.bind(store)}
+        addMsg={store.addMsg.bind(store)}
+        onChangePost={store.onChangePost.bind(store)}
+        onChangeMsg={store.onChangeMsg.bind(store)}
+      />
+    </React.StrictMode>
+  );
+}
+
+
+
+rerenderTree(store.getState())
+store.subscribe(rerenderTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
