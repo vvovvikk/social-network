99ba8cea2e657c209ab4aa8b15fb1ed4d175e51a
Vladimir Alekseenko <skhutor2020@gmail.com>
2023-05-04 20:44:45 +0300

    Friends

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dialogs from './components/dialogs/Dialogs';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Profile from './components/profile/Profile';
+import Friends from './components/navbar/friends/Friends';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
@@ -18,6 +19,7 @@ function App(props) {
             <Route path='/profile' element={<Profile profilePage={props.state.profilePage} newPostText={props.state.profilePage.newPostText} dispatch={props.dispatch} />} />
             <Route exact path='/dialogs' element={<Dialogs dialogsPage={props.state.dialogsPage} newMsg={props.state.dialogsPage.newMsg} dispatch={props.dispatch} />} />
             <Route exact path='/messages' element={<Dialogs dialogsPage={props.state.dialogsPage} newMsg={props.state.dialogsPage.newMsg} dispatch={props.dispatch} />} />
+            <Route exact path='/friends' element={<Friends navBar={props.state.navBar} />} />
           </Routes>
         </div>
       </BrowserRouter>
--- src/components/navbar/friends/Friends.js
+++ src/components/navbar/friends/Friends.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './friends.css'
 import Friend from "./friend/Friend";
+import { NavLink } from "react-router-dom";
 
 let Friends = (props) => {
     return (
         <div className="friends">
-            <h3>Friends</h3>
+            <NavLink to='/friends'>Friends</NavLink> 
             {props.navBar.friends.map(e => <Friend img={e.img} name={e.name} key={e.id} />)}
         </div>
 
